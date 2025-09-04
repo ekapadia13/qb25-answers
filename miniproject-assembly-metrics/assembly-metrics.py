9b1f9f2afe5b6f025c6c524a31418ab8a6ec8cca
ekapadia13 <mng9nd@virginia.edu>
2025-09-04 17:49:49 -0400

    Exercise 5 code update

--- miniproject-assembly-metrics/assembly-metrics.py
+++ miniproject-assembly-metrics/assembly-metrics.py
@@ -5,6 +5,8 @@ import fasta
 my_file = open(sys.argv[1])
 contigs = fasta.FASTAReader(my_file)
 
+contig_length = []
+
 ident_count = 0
 length_total = 0
 for ident, sequence in contigs:
@@ -12,11 +14,22 @@ for ident, sequence in contigs:
     length = len(sequence)
     length_total += length
 
+    contig_length.append(length)
+    contig_length.sort(reverse = True)
+
 average = length_total / ident_count
+
+contig_count = 0
+length_half = length_total / 2
+for contig in contig_length:
+    contig_count += contig
+    if contig_count >= length_half:
+        break
 
 #print(ident_count)
 #print(length_total)
 #print(average)
-
+#print(contig_length)
 
 print(f"Number of contigs: {ident_count} , Total Length: {length_total} , Average Length: {average}")
+print(f"{contig} is the sequence length of the shortest contig at 50% of the total assembly length")
