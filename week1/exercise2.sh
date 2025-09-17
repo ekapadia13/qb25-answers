0fb9620278935187af0e95697cf917c5536594b5
ekapadia13 <mng9nd@virginia.edu>
2025-09-17 18:49:35 -0400

    exercise 2

---
+++ week1/exercise2.sh
@@ -0,0 +1,19 @@
+# Calculate Genes Hg19
+wc hg19-kc.bed
+# Answer = 80,270
+
+# Unique Hg19 Genes
+bedtools intersect -a hg19-kc.bed -b hg16-kc.bed -v > hg19_unique.bed
+# Answer = 42,717
+
+#Hg19 is a newer assembly than Hg16, which means it has more information than older assemblies may have had. Therefore, more genes will appear in Hg19 than Hg16
+
+# Calculate Genes in Hg16
+wc hg16-kc.bed
+# Answer = 21,365
+
+# Unique Hg16 Genes
+bedtools intersect -a hg16-kc.bed -b hg19-kc.bed -v > hg16_unique.bed
+# Answer = 3,460
+
+#Hg16 is older. Therefore, you could have instances such as people thinking alleles of a gene or portions of the same gene were actually different genes. By Hg19, they were realized as only one gene. This could be why we see the difference
