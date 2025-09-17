aba0e8c9c81fce1f1d187b6bfc18962d541f9437
ekapadia13 <mng9nd@virginia.edu>
2025-09-17 18:54:56 -0400

    exercise 2 w sh modified

--- week1/exercise2.sh
+++ week1/exercise2.sh
@@ -16,4 +16,4 @@ wc hg16-kc.bed
 bedtools intersect -a hg16-kc.bed -b hg19-kc.bed -v > hg16_unique.bed
 # Answer = 3,460
 
-#Hg16 is older. Therefore, you could have instances such as people thinking alleles of a gene or portions of the same gene were actually different genes. By Hg19, they were realized as only one gene. This could be why we see the difference
+#Hg16 is older. Therefore, you could have instances such as people thinking alleles of a gene or portions of the same gene were actually different genes. By Hg19, they were realized as only one gene. This could be why we see the difference. Additionally, if you had alternatively spliced transcripts which were used to create the assembly, people may have thought those to be separate genes instead of spliced transcripts of the same gene
