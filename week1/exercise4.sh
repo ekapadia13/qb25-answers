020a359a35c9ec016a088a3ed2c47e26a3bb04e6
ekapadia13 <mng9nd@virginia.edu>
2025-09-19 01:54:44 -0400

    exercise 4 final

--- week1/exercise4.sh
+++ week1/exercise4.sh
@@ -1,16 +1,16 @@
-bedtools intersect -a hg19-kc.bed -b snps-chr1.bed -c > snp_amount | sort -k5,5nr | head
+bedtools intersect -a hg19-kc.bed -b snps-chr1.bed -c > snp_amount | sort -k 5 -nr | head
 # chr1	245912648	246670581	ENST00000490107.6_7	5445
-# Systematic Name = ENSG00000226876.4_13 or ENST00000651181.1_1 (gene vs trancript name)
-# Human Readable Name = SMYD3-AS1
-# Position = hg19 chr1:246,271,939-246,277,106
-# Size = 5,168
-# Exon Count = 5 
-# I think this gene has the most SNPs because .........................
+# Systematic Name = ENST00000490107.6_7
+# Human Readable Name = SMYD3
+# Position = hg19 chr1:245,912,865-246,670,519
+# Size = 757,655
+# Exon Count = 12
+# This is an extremely large gene. It is over 750,000 bp long, with 12 exons. This means there is lots of space for SNPs to occur, probably accounting for the amount of SNPs we see
 
-bedtools sample -i snp_amount -n 20 -seed 42 > snp_subset 
+bedtools sample -i snps-chr1.bed -n 20 -seed 42 > snp_subset 
 bedtools sort -i snp_subset > snp_subset_sorted
 bedtools sort -i hg19-kc.bed > hg19-kc_sorted.bed
-bedtools closest -a snp_subset_sorted -b hg19_kc_sorted_sorted -d -t first > in_out               
-#they are all inside the gene
-
+bedtools closest -a snp_subset_sorted -b hg19-kc_sorted.bed -d -t first > in_out
+# There are 15 inside the gene
+# The range is 1,664-22,944 bq outside the gene
 
